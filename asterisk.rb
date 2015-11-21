db4a746d2d63d51a1d720f726044fc09415ebeef
Adi Linden <adi@adis.ca>
2015-11-21 01:44:50 -0600

    Added basic-pbx patch

--- asterisk.rb
+++ asterisk.rb
@@ -4,6 +4,10 @@ class Asterisk < Formula
   url "http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-13.6.0.tar.gz"
   sha256 "8a01b53c946d092ac561c11b404f68cd328306d0e3b434a7485a11d4b175005a"
 
+  stable do
+    patch :p0, :DATA
+  end
+
   devel do
     url "https://github.com/asterisk/asterisk.git", :branch => "13"
     version "13.7-devel"
@@ -108,10 +112,13 @@ class Asterisk < Formula
     end
 
     system "make", "all", "NOISY_BUILD=yes"
-    system "make", "ASTETCDIR=#{doc}/samples", "install", "samples"
+    system "make", "install"
+    system "make", "ASTETCDIR=#{doc}/samples", "samples"
+    system "make", "ASTETCDIR=#{doc}/basic-pbx", "basic-pbx"
 
     # Replace Cellar references to opt/asterisk
     system "sed", "-i", "", "s#Cellar/asterisk/[^/]*/#opt/asterisk/#", "#{doc}/samples/asterisk.conf"
+    system "sed", "-i", "", "s#Cellar/asterisk/[^/]*/#opt/asterisk/#", "#{doc}/basic-pbx/asterisk.conf"
   end
 
   def plist; <<-EOS.undent
@@ -148,3 +155,122 @@ class Asterisk < Formula
     EOS
   end
 end
+
+__END__
+--- Makefile.orig	2015-11-20 22:02:01.000000000 -0800
++++ Makefile	2015-11-20 22:04:53.000000000 -0800
+@@ -124,7 +124,7 @@
+ space:=$(empty) $(empty)
+ ASTTOPDIR:=$(subst $(space),\$(space),$(CURDIR))
+ 
+-# Overwite config files on "make samples"
++# Overwite config files on "make samples" or other config installation targets
+ OVERWRITE=y
+ 
+ # Include debug and macro symbols in the executables (-g) and profiling info (-pg)
+@@ -652,7 +651,12 @@
+ 	@echo " + configuration files (overwriting any      +"
+ 	@echo " + existing config files), run:              +"
+ 	@echo " +                                           +"
+-	@echo " +               $(mK) samples               +"
++	@echo " + For generic reference documentation:      +"
++	@echo " +   $(mK) samples                           +"
++	@echo " +                                           +"
++	@echo " + For a sample basic PBX:                   +"
++	@echo " +   $(mK) basic-pbx                         +"
++	@echo " +                                           +"
+ 	@echo " +                                           +"
+ 	@echo " +-----------------  or ---------------------+"
+ 	@echo " +                                           +"
+@@ -670,24 +674,15 @@
+ 
+ upgrade: bininstall
+ 
+-# XXX why *.adsi is installed first ?
+-adsi:
+-	@echo Installing adsi config files...
+-	$(INSTALL) -d "$(DESTDIR)$(ASTETCDIR)"
+-	@for x in configs/samples/*.adsi; do \
+-		dst="$(DESTDIR)$(ASTETCDIR)/`$(BASENAME) $$x`" ; \
+-		if [ -f "$${dst}" ] ; then \
+-			echo "Overwriting $$x" ; \
+-		else \
+-			echo "Installing $$x" ; \
+-		fi ; \
+-		$(INSTALL) -m 644 "$$x" "$(DESTDIR)$(ASTETCDIR)/`$(BASENAME) $$x`" ; \
+-	done
+ 
+-samples: adsi
+-	@echo Installing other config files...
+-	@for x in configs/samples/*.sample; do \
+-		dst="$(DESTDIR)$(ASTETCDIR)/`$(BASENAME) $$x .sample`" ;	\
++# Install configuration files from the specified directory
++# Parameters:
++#  (1) the configuration directory to install from
++#  (2) the extension to strip off
++define INSTALL_CONFIGS
++	$(INSTALL) -d "$(DESTDIR)$(ASTETCDIR)"
++	@for x in configs/$(1)/*$(2); do \
++		dst="$(DESTDIR)$(ASTETCDIR)/`$(BASENAME) $$x $(2)`"; \
+ 		if [ -f "$${dst}" ]; then \
+ 			if [ "$(OVERWRITE)" = "y" ]; then \
+ 				if cmp -s "$${dst}" "$$x" ; then \
+@@ -702,7 +696,7 @@
+ 		fi ; \
+ 		echo "Installing file $$x"; \
+ 		$(INSTALL) -m 644 "$$x" "$${dst}" ;\
+-	done
++	done ; \
+ 	if [ "$(OVERWRITE)" = "y" ]; then \
+ 		echo "Updating asterisk.conf" ; \
+ 		sed -e 's|^astetcdir.*$$|astetcdir => $(ASTETCDIR)|' \
+@@ -719,10 +713,28 @@
+ 			"$(DESTDIR)$(ASTCONFPATH)" > "$(DESTDIR)$(ASTCONFPATH).tmp" ; \
+ 		$(INSTALL) -m 644 "$(DESTDIR)$(ASTCONFPATH).tmp" "$(DESTDIR)$(ASTCONFPATH)" ; \
+ 		rm -f "$(DESTDIR)$(ASTCONFPATH).tmp" ; \
+-	fi ; \
++	fi
++endef
++
++# XXX why *.adsi is installed first ?
++adsi:
++	@echo Installing adsi config files...
++	$(INSTALL) -d "$(DESTDIR)$(ASTETCDIR)"
++	@for x in configs/samples/*.adsi; do \
++		dst="$(DESTDIR)$(ASTETCDIR)/`$(BASENAME) $$x`" ; \
++		if [ -f "$${dst}" ] ; then \
++			echo "Overwriting $$x" ; \
++		else \
++			echo "Installing $$x" ; \
++		fi ; \
++		$(INSTALL) -m 644 "$$x" "$(DESTDIR)$(ASTETCDIR)/`$(BASENAME) $$x`" ; \
++	done
++
++samples: adsi
++	@echo Installing other config files...
++	$(call INSTALL_CONFIGS,samples,.sample)
+ 	$(INSTALL) -d "$(DESTDIR)$(ASTSPOOLDIR)/voicemail/default/1234/INBOX"
+ 	build_tools/make_sample_voicemail "$(DESTDIR)/$(ASTDATADIR)" "$(DESTDIR)/$(ASTSPOOLDIR)"
+-
+ 	@for x in phoneprov/*; do \
+ 		dst="$(DESTDIR)$(ASTDATADIR)/$$x" ;	\
+ 		if [ -f "$${dst}" ]; then \
+@@ -741,6 +753,10 @@
+ 		$(INSTALL) -m 644 "$$x" "$${dst}" ;\
+ 	done
+ 
++basic-pbx:
++	@echo Installing basic-pbx config files...
++	$(call INSTALL_CONFIGS,basic-pbx)
++
+ webvmail:
+ 	@[ -d "$(DESTDIR)$(HTTP_DOCSDIR)/" ] || ( printf "http docs directory not found.\nUpdate assignment of variable HTTP_DOCSDIR in Makefile!\n" && exit 1 )
+ 	@[ -d "$(DESTDIR)$(HTTP_CGIDIR)" ] || ( printf "cgi-bin directory not found.\nUpdate assignment of variable HTTP_CGIDIR in Makefile!\n" && exit 1 )
+@@ -1013,6 +1029,7 @@
+ .PHONY: validate-docs
+ .PHONY: _clean
+ .PHONY: ari-stubs
++.PHONY: basic-pbx
+ .PHONY: $(SUBDIRS_INSTALL)
+ .PHONY: $(SUBDIRS_DIST_CLEAN)
+ .PHONY: $(SUBDIRS_CLEAN)
